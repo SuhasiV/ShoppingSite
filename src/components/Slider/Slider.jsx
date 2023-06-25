7645181887335d8ef34ddcd4b375f40e1f1dbf8a
Suhasi Verma <suhasiverma00@gmail.com>
2023-06-25 11:01:53 +0530

    slider complete

---
+++ src/components/Slider/Slider.jsx
@@ -0,0 +1,57 @@
+import React from 'react';
+import './Slider.scss';
+import sliderImg1 from '../../assets/slider-1.jpg';
+import sliderImg2 from '../../assets/slider-2.jpg';
+import sliderImg3 from '../../assets/slider-3.jpg';
+import { BiRightArrow, BiLeftArrow } from 'react-icons/bi';
+import { useState } from 'react';
+
+const Slider = () => {
+  const [currentSlide, setCurrentSlide] = useState(0);
+
+  //const data = [
+  //  {
+  //    id: 1,
+  //    img: { sliderImg1 },
+  //  },
+  //  {
+  //    id: 2,
+  //    img: '../../assets/slider-1.jpg',
+  //  },
+  //  {
+  //    id: 3,
+  //    img: '../../assets/slider-1.jpg',
+  //  },
+  //];
+
+  const prevSlide = () => {
+    setCurrentSlide(currentSlide === 0 ? 2 : currentSlide - 1);
+  };
+
+  const nextSlide = () => {
+    setCurrentSlide(currentSlide === 2 ? 0 : currentSlide + 1);
+  };
+
+  return (
+    <div className="slider">
+      <div
+        className="container"
+        style={{ transform: `translateX(-${currentSlide * 100}vw)` }}
+      >
+        <img src={sliderImg1} alt="" />
+        <img src={sliderImg2} alt="" />
+        <img src={sliderImg3} alt="" />
+      </div>
+      <div className="icons">
+        <div className="icon" onClick={prevSlide}>
+          <BiLeftArrow />
+        </div>
+        <div className="icon" onClick={nextSlide}>
+          <BiRightArrow />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Slider;

09f42901aebf9b8123fd13a145e740b8cdb095f9
Suhasi Verma <suhasiverma00@gmail.com>
2023-06-30 12:41:09 +0530

    added axios, .env, makeRequest.js, imported data into product.jsx

--- src/pages/Product/Product.jsx
+++ src/pages/Product/Product.jsx
@@ -1,58 +1,84 @@
+/* eslint-disable */
 import { useState } from 'react';
 import './Product.scss';
 import { AiOutlineShoppingCart } from 'react-icons/ai';
+import useFetch from '../../hooks/useFetch';
+import { useParams } from 'react-router-dom';
 
 const Product = () => {
-  const [selectedImg, setSelectedImg] = useState(0);
-  const [quantity, setQuantity] = useState(0);
+  const id = useParams().id;
+  const [selectedImg, setSelectedImg] = useState('img');
+  const [quantity, setQuantity] = useState(1);
 
-  const images = ['../watchCat.jpg', '../winterCat.jpg'];
+  const { data, loading, error } = useFetch(`/products/${id}?populate=*`);
 
   return (
     <div className="product">
-      <div className="left">
-        <div className="images">
-          <img src={images[0]} alt="" onClick={() => setSelectedImg(0)} />
-          <img src={images[1]} alt="" onClick={() => setSelectedImg(1)} />
-        </div>
-        <div className="mainImg">
-          <img src={images[selectedImg]} alt="" />
-        </div>
-      </div>
+      {loading ? (
+        'loading'
+      ) : (
+        <>
+          <div className="left">
+            <div className="images">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img?.data?.attributes?.url
+                }
+                alt=""
+                onClick={() => setSelectedImg('img')}
+              />
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.img2?.data?.attributes?.url
+                }
+                alt=""
+                onClick={() => setSelectedImg('img2')}
+              />
+            </div>
+            <div className="mainImg">
+              <img
+                src={
+                  process.env.REACT_APP_UPLOAD_URL +
+                  data?.attributes?.[selectedImg]?.data?.attributes?.url
+                }
+                alt=""
+              />
+            </div>
+          </div>
 
-      <div className="right">
-        <h1>Title</h1>
-        <span className="price">$199</span>
-        <p>
-          {' '}
-          udscscndicnsdjcnsdjvcnsdijcnsiucsdjndihvbdsicnsiuchneiuchsjcnsdjchbsicujnacuhencijsbdhcbweucbicujnsiucbiweucsjnbsiucnbsdceuicnsucidcnd
-          udscscndicnsdjcnsdjvcnsdijcnsiucsdjndihvbdsicnsiuchneiuchsjcnsdjchbsicujnacuhencijsbdhcbweucbicujnsiucbiweucsjnbsiucnbsdceuicnsucidcnd
-        </p>
-        <div className="quantity">
-          <button
-            onClick={() => setQuantity(prev => (prev === 1 ? 1 : prev - 1))}
-          >
-            -
-          </button>
-          {quantity}
-          <button onClick={() => setQuantity(prev => prev + 1)}>+</button>
-        </div>
-        <button className="add">
-          <AiOutlineShoppingCart />
-          ADD TO CART
-        </button>
-        <div className="info">
-          <span>Vendor: Polo</span>
-          <span>Product Type: T Shirt</span>
-        </div>
-        <hr />
-        <div className="info">
-          <span>DESCRIPTION</span>
-          <hr />
-          <sapn> ADDITIONAL INFO</sapn>
-          <hr />
-        </div>
-      </div>
+          <div className="right">
+            <h1>{data?.attributes?.title}</h1>
+            <span className="price">${data?.attributes?.price}</span>
+            <p>{data?.attributes?.desc}</p>
+            <div className="quantity">
+              <button
+                onClick={() => setQuantity(prev => (prev === 1 ? 1 : prev - 1))}
+              >
+                -
+              </button>
+              {quantity}
+              <button onClick={() => setQuantity(prev => prev + 1)}>+</button>
+            </div>
+            <button className="add">
+              <AiOutlineShoppingCart />
+              ADD TO CART
+            </button>
+            <div className="info">
+              <span>Vendor: Polo</span>
+              <span>Product Type: T Shirt</span>
+            </div>
+            <hr />
+            <div className="info">
+              <span>DESCRIPTION</span>
+              <hr />
+              <sapn> ADDITIONAL INFO</sapn>
+              <hr />
+            </div>
+          </div>
+        </>
+      )}
     </div>
   );
 };
